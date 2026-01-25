896e6ea9fe2047800559f2d720be892b423c2a92
Facundo Tobio <facundohectortobio@gmail.com>
2026-01-25 14:02:41 -0300

    subtitle updated + navigation test fixed

--- cypress/e2e/PruebasAutomatizadas/navigationtest.cy.js
+++ cypress/e2e/PruebasAutomatizadas/navigationtest.cy.js
@@ -4,17 +4,19 @@ context('NavigationToMiAgenda', () => {
   it('Verifica la presencia de un elemento después de hacer clic en "Mi agenda"', () => {
     // Navega a la URL principal y espera 5 segundos para que el navegador termine de cargar
     cy.visit('https://facundo-tobio-portfolio.vercel.app/')
-    cy.wait(3000)
+    cy.wait(5000)
 
     // Haz clic en el botón "Mi agenda" y espera 5 segundos para dar tiempo a la página de Calendly para cargar
     cy.contains('button', 'Mi agenda').click()
-    cy.wait(3000)
+    cy.wait(6000)
 
     // Usar cy.origin() para indicar que estos comandos se ejecutarán en el nuevo origen
-    cy.origin('https://calendly.com', () => {
+    cy.origin('https://calendly.com/facundotobio', () => {
       // Espera 10 segundos y luego verifica la presencia del elemento específico en la página de Calendly
       cy.wait(6000)
-      cy.get('[id="root"] > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div > h1').should('exist')
+      // cy.get('[id="root"] > div > div > div > div > div > div > div > div:nth-child(2) > div > div > div > div > div > h1').should('exist')
+      cy.contains('Meeting whit Facundo Tobio', { timeout: 10000 }).should('exist')
+
     })
   })
 })
